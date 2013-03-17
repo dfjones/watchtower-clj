e708e93530dac9f24502d4b593571f0f9aa353cb
Doug Jones <doug@dfjones.net>
2013-03-17 16:38:01 -0400

    Clojure 1.5

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject watchtower-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler watchtower-clj.handler/app}
